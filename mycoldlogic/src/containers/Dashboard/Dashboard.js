3cc966539f38d52e47474da68c0ae3f1df810964
Luis Espinal <lespinalrivera@gmail.com>
2018-05-08 22:14:58 -0700

    Start media queries for screen breakpoints

--- mycoldlogic/src/containers/Dashboard/Dashboard.js
+++ mycoldlogic/src/containers/Dashboard/Dashboard.js
@@ -3,13 +3,13 @@ import './Dashboard.css';
 import Announcements from '../../components/Announcements/Announcements';
 import Nav from '../../components/Nav/Nav';
 import AskManagerForm from './AskManagerForm/AskManagerForm';
-import ColdLogicLogo from '../../assets/ColdLogicLogo';
+
 class Dashboard extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      error: false
+      error: true
     };
   }
   render(){
@@ -18,8 +18,6 @@ class Dashboard extends Component {
           <div className='Dashboard-flex-container'>
               <Nav navLinks={this.props.navLinks}/>
               <AskManagerForm/>
-              <ColdLogicLogo color={'#FEE222'} alt="Look Cody, no hands man!"/>
-              <ColdLogicLogo color={'#F222EE'}/>
           </div>
       </div>);
   }
