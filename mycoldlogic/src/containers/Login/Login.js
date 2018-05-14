42f2b2f0ffee68ef91e1072b26fea941c43e66c8
Luis Espinal <lespinalrivera@gmail.com>
2018-05-14 12:55:48 -0700

    add Login form phone support up to width 600px

--- mycoldlogic/src/containers/Login/Login.js
+++ mycoldlogic/src/containers/Login/Login.js
@@ -37,7 +37,7 @@ class Login extends Component {
       login = <RequestPassword />
     }
     return(
-      <div>
+      <div className="login">
         <Header />
         {login}
       </div>
