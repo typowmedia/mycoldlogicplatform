e3b397a7fba48742195e34ad8cc1b098a6cb6481
Luis Espinal <lespinalrivera@gmail.com>
2018-05-15 22:44:45 -0700

    style dashboard at regular monitor media query

--- mycoldlogic/src/containers/Dashboard/AskManagerForm/AskManagerForm.js
+++ mycoldlogic/src/containers/Dashboard/AskManagerForm/AskManagerForm.js
@@ -8,11 +8,13 @@ class AskManagerForm extends Component {
         <h3>Ask a Manager</h3>
         <hr/>
         <form>
+            <div className="select-manager">
           <select>
-            <option>Dan Imbery</option>
-            <option>Laurel O'Donnell</option>
-            <option>Shanda Hope</option>
+            <option value="option1">Dan Imbery</option>
+            <option value="option2">Laurel O'Donnell</option>
+            <option value="option3">Shanda Hope</option>
           </select>
+        </div>
           <div>Subject:<input type='text' placeholder='Write your subject here'/></div>
           <label htmlFor='ask-manager-message'>Message:</label>
           <textarea rows='4' name='ask-manager-message'></textarea>
