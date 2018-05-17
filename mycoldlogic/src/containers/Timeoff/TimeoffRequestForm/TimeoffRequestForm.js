7a5716815cf7afa75b3c51b3119c16a59c58af13
Luis Espinal <lespinalrivera@gmail.com>
2018-05-16 22:23:31 -0700

    add more styling to Time off Request

--- mycoldlogic/src/containers/Timeoff/TimeoffRequestForm/TimeoffRequestForm.js
+++ mycoldlogic/src/containers/Timeoff/TimeoffRequestForm/TimeoffRequestForm.js
@@ -4,7 +4,7 @@ import './TimeoffRequestForm.css';
 const TimeoffRequestForm = ( props ) => {
   return (
     <form className='TimeoffRequestForm'>
-      <div>
+      <div className='pickdate'>
         <label htmlFor='fromDate'>From</label>
         <input type='date' name='fromDate' />
         <label htmlFor='toDate'>To</label>
@@ -13,17 +13,17 @@ const TimeoffRequestForm = ( props ) => {
       <div>
         <label htmlFor='reason'>Reason</label>
         <select name='reason'>
-          <option>Vacation</option>
-          <option>Unpaid Vacation</option>
-          <option>Appointment</option>
-          <option>Other</option>
+          <option value='option 1'>Vacation</option>
+          <option value='option 2'>Unpaid Vacation</option>
+          <option value='option 3'>Appointment</option>
+          <option value='option 4'>Other</option>
         </select>
       </div>
       <div>
-        <label htmlFor='reasonDetail'>Please give more detail below</label>
-        <textarea rows='4' name='reasonDetail' onChange={props.updateReason}>{props.reasonDetail}</textarea>
+        <label htmlFor='reasonDetail'>More Detail:</label>
+        <textarea rows='4' name='reasonDetail' onChange={props.updateReason} placeholder='Please give more details here'>{props.reasonDetail}</textarea>
       </div>
-      <button onClick={props.submitTimeoffRequest}>Submit Time off</button>
+      <button className="mainBtn" onClick={props.submitTimeoffRequest}>Submit Time off</button>
     </form>
   );
 };
