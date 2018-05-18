91424eb2d89a556215b0bfe94e53e931b7fd0115
crathor <codyrathor@gmail.com>
2018-05-17 18:34:31 -0700

    add animation to menu and button

--- mycoldlogic/src/components/Header/UserMenu/UserMenu.js
+++ mycoldlogic/src/components/Header/UserMenu/UserMenu.js
@@ -16,30 +16,16 @@ class UserMenu extends Component {
   };
 
   render(){
-    const style = {
-      position: 'absolute',
-      top: '55px',
-      width: '200px',
-      height: `${!this.state.toggleMenu ? '0px' : 'auto'}`,
-      left: '-10px',
-      zIndex: '1',
-      listStyle: 'none',
-      background: '#CCC',
-      overflow: 'hidden',
-      transition: '2s'
-    }
     let flip = {
       transform: "rotate(180deg)",
-      transition: ".5s"
     };
-    let unflip = {
-      transition: ".5s"
+    let slide = {
+      transform: 'scale(1)',
     };
-
     return(
       <div className='UserMenu'>
-      <button style={this.state.toggleMenu ? flip : unflip} onClick={this.toggleMenu}><i className="fas fa-bars"></i></button>
-      <ul style={style}>
+      <button style={this.state.toggleMenu ? flip : null} onClick={this.toggleMenu}><i className="fas fa-bars"></i></button>
+      <ul style={this.state.toggleMenu ? slide : null}>
         <li><Link to='/' onClick={this.toggleMenu}>Dashboard</Link></li>
         <li><Link to='/my-schedule' onClick={this.toggleMenu}>My Schedule</Link></li>
         <li><Link to='/timeoff' onClick={this.toggleMenu}>Request Timeoff</Link></li>
