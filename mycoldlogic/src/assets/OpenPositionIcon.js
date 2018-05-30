877fc4ab607088d2b17a760e9732743588ae3559
Luis Espinal <lespinalrivera@gmail.com>
2018-05-29 23:15:24 -0700

    corrected Openposition Icon to 100x100px styled it better

--- mycoldlogic/src/assets/OpenPositionIcon.js
+++ mycoldlogic/src/assets/OpenPositionIcon.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 const OpenPositionIcon = (props) => {
-    return <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 121.43">
+    return <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
         <title>OpenPositionIcon</title>
-        <polygon points="28.57 92.86 71.43 92.86 71.43 50 100 50 50 0 0 50 28.57 50 28.57 92.86" fill={props.color}/>
-        <rect y="107.14" width="100" height="14.29" fill={props.color}/>
+        <path d="M32.3,82.1h35.4c0.8,0,1.4-0.6,1.4-1.4V45.4c0-0.8,0.6-1.4,1.4-1.4h18.3c2.1,0,3.2-2.6,1.7-4.1L51,0.4
+	c-0.5-0.5-1.4-0.5-2,0L9.5,39.9C8,41.4,9.1,44,11.2,44h18.3c0.8,0,1.4,0.6,1.4,1.4v35.4C30.9,81.5,31.5,82.1,32.3,82.1z" fill={props.color}/>
+        <path d="M2.4,88h95.2c1.3,0,2.4,1.1,2.4,2.4v7.2c0,1.3-1.1,2.4-2.4,2.4H2.4C1.1,100,0,98.9,0,97.6v-7.2
+	C0,89.1,1.1,88,2.4,88z" fill={props.color}/>
     </svg>
 };
 
